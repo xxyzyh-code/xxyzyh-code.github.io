4f4fd42e40cfc2433015e46d30b7be8f251ef945
xxyzyh-code <xxyzyh@gmail.com>
2025-11-22 20:46:00 +0800

    Delete assets/js/main.js

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initializeDOMElements } from './Config.js';
-import './StateAndUtils.js';
-import './LrcParser.js';
-import './AudioEngine.js';
-import './PlayerCore.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-    initializeDOMElements();
-});
